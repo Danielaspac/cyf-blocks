299cd87a361beda4dd2afded2193771f2de41e55
Danielaspac <dano4ka3md@mail.ru>
2022-07-26 22:06:04 +0100

    ok

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-var current;
+var current, array;
 
 
-var array = ['https://oceanvisions.org/wp-content/uploads/2021/09/joseph-barrientos-oQl0eVYd_n8-unsplash-Custom.jpg', 'https://image.shutterstock.com/image-photo/beach-tropical-sea-600w-664029658.jpg', 'https://image.shutterstock.com/image-photo/coconut-palm-trees-grow-on-600w-723806818.jpg'];
-let element_list = document.getElementById('list');
-let new_img = document.createElement('img');
+array = ['https://oceanvisions.org/wp-content/uploads/2021/09/joseph-barrientos-oQl0eVYd_n8-unsplash-Custom.jpg', 'https://image.shutterstock.com/image-photo/beach-tropical-sea-600w-664029658.jpg', 'https://image.shutterstock.com/image-photo/coconut-palm-trees-grow-on-600w-723806818.jpg'];
 let element_image = document.getElementById('image');
-element_image.setAttribute("src", array[0]);
-
-element_list.appendChild(new_img);
+element_image.setAttribute("src", array.shift());
 
 
 document.getElementById('button').addEventListener('click', (event) => {
   current = array.pop();
   let element_image2 = document.getElementById('image');
-  element_image2.setAttribute("src", array[1]);
+  element_image2.setAttribute("src", current);
   array.unshift(current);
 
 });
@@ -21,7 +17,7 @@ document.getElementById('button').addEventListener('click', (event) => {
 document.getElementById('button2').addEventListener('click', (event) => {
   current = array.shift();
   let element_image3 = document.getElementById('image');
-  element_image3.setAttribute("src", array[2]);
+  element_image3.setAttribute("src", current);
   array.push(current);
 
 });
